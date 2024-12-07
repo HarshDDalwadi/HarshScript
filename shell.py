74db7b7d48e937ff38eb7ec8ce24d2137bd9a38e
HarshDDalwadi <102744867+HarshDDalwadi@users.noreply.github.com>
2024-12-07 13:59:38 +0530

    UPDATE: Enhanced the terminal interface

--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 import harshScript
 
 while True:
-	text = input("harshScript >> ")
+	text = input("harshScript >>> ")
 	result, error = harshScript.run('<stdin>', text)
 	if error: print(error.as_string())
 	else: print(result)
